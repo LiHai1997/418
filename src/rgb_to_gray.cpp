710cd6e1959e74a04eea3cc9e4a09de00d752dd3
LH <1550787615@qq.com>
2020-01-16 19:53:58 -0500

    gray bug

--- src/rgb_to_gray.cpp
+++ src/rgb_to_gray.cpp
@@ -9,9 +9,10 @@ void rgb_to_gray(
   gray.resize(height*width);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
-  for (int i = 0; i < (int)gray.size(); i++) {
+  int length = gray.size();
+  for (int i = 0; i < length; i++) {
 	  // more precise formula from stackoverflow
-	  gray[i] = (int)( (double)rgb[i * 3] * 0.2126 + (double)rgb[i * 3 + 1] * 0.7152 + (double)rgb[i * 3 + 2] * 0.0722 );
+	  gray[i] = (int)( (float)rgb[i * 3] * 0.2126 + (float)rgb[i * 3 + 1] * 0.7152 + (float)rgb[i * 3 + 2] * 0.0722 );
   }
   ////////////////////////////////////////////////////////////////////////////
 }
